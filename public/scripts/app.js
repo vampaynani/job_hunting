e577876e96233e5c1adf78259f871d3dfce3774e
Gustavo Luna <gustavolunalimon@gmail.com>
2018-11-09 21:27:28 -0600

    s

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,73 +19,73 @@ function initializate(){
 
     send.addEventListener('click', function(e){
         e.preventDefault(e);
+        console.log('enviando')
 
-        var inputnombre = this.nombre.value,
-        var inputpaterno = this.paterno.value;
-        var inputmaterno = this.materno.value;
+
+        var inputnombre = document.getElementsByName('nombre')[0].value;
+        var inputpaterno = document.getElementsByName('paterno')[0].value;
+        var inputmaterno = document.getElementsByName('materno')[0].value;
         
-        var inputcalle = this.calle.value;
-        var inputcolonia = this.colonia.value;
-        var inputdelegacion = this.delegacion.value;
-        var inputcp = this.cp.value;
+        var inputcalle = document.getElementsByName('calle')[0].value;
+        var inputcolonia = document.getElementsByName('colonia')[0].value;
+        var inputdelegacion = document.getElementsByName('delegacion')[0].value;
+        var inputcp = document.getElementsByName('cp')[0].value;
         
-        var inputedad = this.edad.value;
+        var inputedad = document.getElementsByName('edad')[0].value;
         
-        var inputestado = this.estado.value;
+        var inputestado = document.getElementsByName('estado')[0].value;
         
-        var inputhijos = this.hijos.value;
+        var inputhijos = document.getElementsByName('hijos')[0].value;
         
-        var inputestudio = this.estudio.value;
+        var inputestudio = document.getElementsByName('estudio')[0].value;
 
-        var inputtatuaje = this.tatuaje.value;
+        var inputtatuaje = document.getElementsByName('tatuaje')[0].value;
 
-        var inputempresa = this.empresa.value;
-        var inputseparacion = this.separacion.value;
-        var inputpuesto = this.puesto.value;
-        var inputtiempo = this.tiempo.value;
+        var inputempresa = document.getElementsByName('empresa')[0].value;
+        var inputseparacion = document.getElementsByName('separacion')[0].value;
+        var inputpuesto = document.getElementsByName('puesto')[0].value;
+        var inputtiempo = document.getElementsByName('tiempo')[0].value;
 
-        var inputnombre1 = this.nombre1.value;
-        var inputnombre2 = this.nombre2.value;
-        var inputnombre3 = this.nombre3.value;
+        var inputnombre1 = document.getElementsByName('nombre1')[0].value;
+        var inputnombre2 = document.getElementsByName('nombre2')[0].value;
+        var inputnombre3 = document.getElementsByName('nombre3')[0].value;
 
-        var inputtel1 = this.tel1.value;
-        var inputtel2 = this.tel2.value;
-        var inputtel3 = this.tel3.value;
+        var inputtel1 = document.getElementsByName('tel1')[0].value;
+        var inputtel2 = document.getElementsByName('tel2')[0].value;
+        var inputtel3 = document.getElementsByName('tel3')[0].value;
 
-        var inputocupacion1 = this.ocupacion1.value;
-        var inputocupacion2 = this.ocupacion2.value;
-        var inputocupacion3 = this.ocupacion3.value;
+        var inputocupacion1 = document.getElementsByName('ocupacion1')[0].value;
+        var inputocupacion2 = document.getElementsByName('ocupacion1')[0].value;
+        var inputocupacion3 = document.getElementsByName('ocupacion1')[0].value;
 
-        var inputexpv = this.ventas.value;
-        var inputexpatc = this.atc.value;
+        var inputexpv = document.getElementsByName('expv')[0].value;
+        var inputexpatc = document.getElementsByName('expatc')[0].value;
 
-        var inputlunesm = this.lunesm.value;
-        var inputlunesv = this.lunesv.value;
+        var inputlunesm = document.getElementsByName('lunesm')[0].value;
+        var inputlunesv = document.getElementsByName('lunesv')[0].value;
 
-        var inputmartessm = this.martesm.value;
-        var inputmartesv = this.martesv.value;
+        var inputmartesm = document.getElementsByName('martesm')[0].value;
+        var inputmartesv = document.getElementsByName('martesv')[0].value;
 
-        var inputmiercolesm = this.miercolesm.value;
-        var inputmiercolesv = this.miercolesv.value;
+        var inputmiercolesm = document.getElementsByName('miercolesm')[0].value;
+        var inputmiercolesv = document.getElementsByName('miercolesv')[0].value;
 
-        var inputjuevesm = this.juevesm.value;
-        var inputjuevesv = this.juevesv.value;
+        var inputjuevesm = document.getElementsByName('juevesm')[0].value;
+        var inputjuevesv = document.getElementsByName('juevesv')[0].value;
 
-        var inputviernesm = this.viernesm.value;
-        var inputviernesv = this.viernesv.value;
+        var inputviernesm = document.getElementsByName('viernesm')[0].value;
+        var inputviernesv = document.getElementsByName('viernesv')[0].value;
 
-        var inputsabadom = this.sabadom.value;
-        var inputsabadov = this.sabadov.value;
+        var inputsabadom = document.getElementsByName('sabadom')[0].value;
+        var inputsabadov = document.getElementsByName('sabadov')[0].value;
 
-        var inputdomingom = this.domingom.value;
-        var inputdomingov = this.domingov.value;
+        var inputdomingom = document.getElementsByName('domingom')[0].value;
+        var inputdomingov = document.getElementsByName('domingov')[0].value;
 
-        var inputcv = this.cv.value;
-        var inputine = this.ine.value;
+        var inputcv = document.getElementsByName('cv')[0].value;
+        var inputine = document.getElementsByName('ine')[0].value;
 
-        var Data = this.formulario.value;
-
-        function Data ( 
+        const Data = {
             inputnombre,
             inputpaterno,
             inputmaterno,
@@ -129,8 +129,6 @@ function initializate(){
             inputdomingov,
             inputcv,
             inputine,
-            ){
-
         }
 
         api.submitForm(Data);
