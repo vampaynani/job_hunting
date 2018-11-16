8afb244ca57494ecd53364e762499739aea89c7f
Grimmgalohm <cesarlunalimon@gmail.com>
2018-11-15 18:44:25 -0600

    inputs, app y render.js

--- db/postulantSchema.js
+++ db/postulantSchema.js
@@ -5,53 +5,53 @@ const postulantSchema = mongoose.Schema({
     materno:{type: String},
     paterno:{type: String},
     calle:{type: String},
-    colonia:{type:String},
-    delegacion:{type:String},
-    cp:{type:Number},
-    edad:{type:Number},
-    civil:{type:String},
-    hijos:{type:String}, //Debe ser booleano
-    estudios:{type:String},
-    tatuaje:{type:String}, //Debe ser booleano
-    empresa:{type:String},
-    separacion:{type:String},
-    puesto:{type:String},
-    tiempo:{type:String},
+    colonia:{type: String},
+    delegacion:{type: String},
+    cp:{type: Number},
+    edad:{type: Number},
+    civil:{type: String},
+    hijos:{type: String}, //Se deja String
+    estudios:{type: String},
+    tatuaje:{type: String}, //Se deja String
+    empresa:{type: String},
+    separacion:{type: String},
+    puesto:{type: String},
+    tiempo:{type: String},
     referencias:[{
-        nombre:{type:String},
-        numero:{type:String},
-        ocupacion:{type:String},
+        nombre:{type: String},
+        numero:{type: String},
+        ocupacion:{type: String},
     }],
-    exp_mostrador:{type:String},//Debe ser booleano
-    exp_atc:{type:String},//Debe ser booleano
+    exp_mostrador:{type: String}, //Se deja String
+    exp_atc:{type: String}, //Se deja String
     disponibilidad: {
         Lunes: {
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         },
         Martes: {
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         },
         Miercoles: {
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         },
         Jueves:{
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         },
         Viernes:{
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         },
         Sabado: {
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         },
         Domingo: {
-            matutino: String,//Debe ser booleano
-            vespertino: String//Debe ser booleano
+            matutino: Boolean,
+            vespertino: Boolean
         }
     },
     created: {type:Date, default:Date.now}
